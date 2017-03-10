f2ac2e593a189655b8e1fcd2170b47781c907666
Kevin Bosman <bosmankevin89@gmail.com>
2017-03-09 23:52:39 -0600

    Changed code to use resume JSON object.

--- app/templates/resume/experience.module.js
+++ app/templates/resume/experience.module.js
@@ -3,22 +3,52 @@
  */
 (function () {
     angular.module('resume-module', [])
-            .controller('ExperiencePanelController',
+            .controller('ResumeController',
                     function ($scope, $http) {
-                        $ctrl = this;
-                        
-                        $http.get('app/data/test.json')
+                        ctrl = this;
+
+                        ctrl.resumeJSON = {};
+
+                        $http.get('app/data/resume.json')
                                 .then(function (res) {
-                                    $ctrl.data = res.data;
+                                    ctrl.resumeJSON = res.data;
                                 });
+
+                        //basic
+                        ctrl.getBasicInfo = function () {
+                            return ctrl.resumeJSON["basic"];
+                        };
+                        
+                        //education
+                        ctrl.getEducation = function () {
+                            return ctrl.resumeJSON["education"];
+                        };
+                        
+                        //skills
+                        ctrl.getSkills = function () {
+                            return ctrl.resumeJSON["skills"];
+                        };
+
+                        //work
+                        ctrl.getJobExperiences = function () {
+                            return ctrl.resumeJSON["work"];
+                        };
                     }
             )
-            .component('experienceList',
+            .component('jobExperienceList',
                     {
                         restrict: 'E'
                         , bindings: {
                             data: '<'
                         }
                         , templateUrl: 'app/templates/resume/fragments/job-experience-list.html'
+                    })
+            .component('jobExperience',
+                    {
+                        restrict: 'E'
+                        , bindings: {
+                            data: '<'
+                        }
+                        , templateUrl: 'app/templates/resume/fragments/job-experience.html'
                     });
 })();
